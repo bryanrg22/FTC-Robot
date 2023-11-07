f05fb1f7e7bacac4bb0de2968ddbb363318b34b1
Bryan Eduardo Ramirez-Gonzalez <131308308+bryanram2024@users.noreply.github.com>
2023-11-07 13:41:00 -0800

    Update StarterBot2024Telop.java

--- StarterBot2024Telop.java
+++ StarterBot2024Telop.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode; hello
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.Servo;
