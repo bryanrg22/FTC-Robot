d36b071105635004a12f039585b2947e239e097d
Bryan Eduardo Ramirez-Gonzalez <131308308+bryanram2024@users.noreply.github.com>
2023-12-01 14:03:11 -0800

    Change of Name

--- ManualMode.java
+++ ManualMode.java
@@ -37,9 +37,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Starter Bot 2024", group="Iterative Opmode")
+@TeleOp(name="ManualMode", group="Iterative Opmode")
 
-public class StarterBot2024Teleop extends OpMode
+public class ManualMode extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
